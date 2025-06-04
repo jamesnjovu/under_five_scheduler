3e2942e5e97a3e90d06591b522abb75b4d7c7335
James <njovujames@gmail.com>
2025-06-04 16:52:37 +0200

    a

--- lib/app_web/live/unauth/user_registration_live.ex
+++ lib/app_web/live/unauth/user_registration_live.ex
@@ -111,7 +111,6 @@ defmodule AppWeb.UserRegistrationLive do
                     required
                     options={[
                       {"Parent", "parent"},
-                      {"Healthcare Provider", "provider"}
                     ]}
                     class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm transition-colors duration-200"
                   />
